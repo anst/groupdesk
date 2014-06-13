b8ddfaedd1a543a306b5f18dd9431fa3c9f989b2
chufanl <chufan.liang@gmail.com>
2014-06-13 16:46:59 -0500

    WOW ANDY

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__).'/app/app.php';
 
 $app = new app('app');
 $app->route('/', function($app) { 
-	return $app->render("home.html",['sup','sohacks swag']);
+	return $app->render("home.html",['sup'=>'sohacks swag']);
 });
 $app->route('/<string>', function($app, $api_query) {
 });
