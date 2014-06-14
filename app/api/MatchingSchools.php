471078a41d8e7ae78d28807e6d0d16b40a05ac1e
M_bot <miguel_lego@yahoo.com>
2014-06-13 21:37:44 -0500

    MatchingSchools endpoint bugfix

--- app/api/MatchingSchools.php
+++ app/api/MatchingSchools.php
@@ -2,11 +2,11 @@
 require_once 'class/School.php';
 
 //Example /api/MatchingSchools.php?name=Seven Lak
-if (!isset($_POST['name']) || strlen($_POST['name'] < 5)) {
+if (!isset($_POST['name']) || strlen($_POST['name']) < 5) {
     return;
 }
 School::retrieve();
-return School::getMatchingSchools(filter_input(INPUT_POST, $_POST['name']));
+print_r(School::getMatchingSchools($_POST['name']));
 
 
 
