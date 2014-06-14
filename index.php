9074858d2a85718e0b51c7eb0560ebdb0f6c777a
blacksmithgu <blacksmithgu@gmail.com>
2014-06-14 02:58:34 -0500

    Fixed index.php.

--- index.php
+++ index.php
@@ -2,38 +2,44 @@
 require_once dirname(__FILE__).'/app/app.php';
 require_once dirname(__FILE__) . '/app/api/autoloader.php';
 
-
 $app = new app('app');
+
 $app->route('/', function($app) { 
 	return $app->render("home.html",[]);
 });
+
 $app->route('/about', function($app) { 
 	return $app->render("about.html",[]);
 });
+
 $app->route('/login', function($app) { 
 	return $app->render("login.html",[]);
 });
+
 $app->route('/teachers', function($app) { 
 	return $app->render("teachers.html",[]);
 });
+
 $app->route('/students', function($app) { 
 	return $app->render("students.html",[]);
 });
+
 $app->route('/students_app', function($app) { 
   return $app->render("students_app.html",[]);
 });
+
 $app->route('/teachers_app', function($app) { 
   return $app->render("teachers_app.html",[]);
 });
+
 $app->route('/students_app/room', function($app) { 
   return $app->render("students_room_view.html",[]);
 });
+
 $app->route('/students_class', function($app) { 
   return $app->render("students_class_view.html",[]);
 });
 
-$app->route("/api/user", function($app, $api_query) {
-    
 $app->route('/wysiwyg', function($app) { 
   return $app->render("wysiwyg.html",[]);
 });
