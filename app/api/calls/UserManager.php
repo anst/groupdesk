e42fac0018c248bca724e98b4b45f566a9c3152e
M_bot <miguel_lego@yahoo.com>
2014-06-14 00:11:35 -0500

    Added /api/gravatar endpoint

--- app/api/calls/UserManager.php
+++ app/api/calls/UserManager.php
@@ -16,11 +16,35 @@ class UserManager {
         
         $app->route("/api/current", function($app) {
             $user = User::current();
-            if(is_null($user))
+            if (is_null($user)) {
                 echo "null";
-            else
+            } else {
                 echo $user->json(true);
+            }
         });
+        
+        $app->route("/api/gravatar", function($app) {
+            $user = User::current();
+            if (is_null($user)) {
+                echo "default.png";
+            } else {
+                echo get_gravatar(User::current()->get('email'), $_GET['img']);
+            }
+        });
+    }
+    
+    function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+        $url = 'http://www.gravatar.com/avatar/';
+        $url .= md5( strtolower( trim( $email ) ) );
+        $url .= "?s=$s&d=$d&r=$r";
+        if ( $img ) {
+            $url = '<img src="' . $url . '"';
+            foreach ($atts as $key => $val) {
+                $url .= ' ' . $key . '="' . $val . '"';
+            }
+            $url .= ' />';
+        }
+        return $url;
     }
 
 }
